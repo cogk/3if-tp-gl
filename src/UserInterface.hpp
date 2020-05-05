451c64fde54c146230ab5cd322bbc5a8b199eba4
Corentin Forler <cogk42@users.noreply.github.com>
2020-05-05 15:58:28 +0200

    improved UserInterface

--- src/UserInterface.hpp
+++ src/UserInterface.hpp
@@ -24,44 +24,108 @@
 
 #include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+namespace UserInterface
+{
+const int InvalidMenuChoice = -1;
+int MaxWidth = 32;
+
+// fonctions utilitaires
+
+// lire un entier
+struct ReadIntegerResult
+{
+    int value;
+    bool valid;
+};
+ReadIntegerResult
+readInteger(std::istream &inputStream = std::cin)
+{
+    std::string input;
+    std::getline(inputStream, input);
+
+    ReadIntegerResult res;
+
+    try
+    {
+        const int value = std::stoi(input);
+        res.value = value;
+        res.valid = true;
+    }
+    catch (std::invalid_argument)
+    {
+        res.value = 0;
+        res.valid = false;
+    }
+
+    return res;
+}
+
+// centrer une chaine de caractères
 std::string
-stringCenter(std::string const &str, int targetSize)
+util_stringCenter(std::string const &str, int targetSize)
 {
     assert(targetSize >= 0);
     int padding = targetSize - str.length();
     if (padding > 0)
     {
         int half = padding / 2;
-        return std::string(half, ' ') + str + std::string(half, ' ');
+        return std::string(half, ' ') + str + std::string(padding - half, ' ');
     }
     return str;
 }
 
-namespace UserInterface
+void banner(std::string text, bool thick = false, std::ostream &out = std::cout)
 {
-int menu(std::string menuName, std::vector<std::string> entries)
-{
-    std::cout << std::endl;
-    std::cout << ANSI_BOLD_ON << ANSI_BG_YELLOW << ANSI_FG_BLACK << stringCenter(menuName, menuName.length() + 6) << ANSI_FG_RESET << ANSI_BG_RESET << ANSI_BOLD_OFF << std::endl;
-    std::cout << std::endl;
+    const int width = MaxWidth;
+    // const int width = text.length() + 2 * 3;
 
-    size_t i = 0;
+    if (thick)
+        out << ANSI_BG_YELLOW << ANSI_FG_BLACK << std::string(width, ' ') << ANSI_FG_RESET << ANSI_BG_RESET << std::endl;
+
+    out << ANSI_BG_YELLOW << ANSI_FG_BLACK << util_stringCenter(text, width) << ANSI_FG_RESET << ANSI_BG_RESET << std::endl;
+
+    if (thick)
+        out << ANSI_BG_YELLOW << ANSI_FG_BLACK << std::string(width, ' ') << ANSI_FG_RESET << ANSI_BG_RESET << std::endl;
+}
+
+/**
+ * Attention :
+ * - ne renverra JAMAIS 0
+ * - renvoie un entier entre 1 et N lorsque le choix est valide
+ * - renvoie UserInterface::InvalidMenuChoice sinon
+ */
+int menu(std::string menuName, std::vector<std::string> entries, std::ostream &out = std::cout)
+{
+    UserInterface::banner(menuName, false, out);
+    out << std::endl;
+
+    int i = 0;
     for (auto const &entry : entries)
     {
         i += 1;
-        std::cout << ANSI_FG_BLUE << std::setw(4) << i << ". " << ANSI_FG_RESET << ANSI_BOLD_ON << entry << ANSI_BOLD_OFF << std::endl;
+        out << ANSI_FG_BLUE << std::setw(4) << i << ". " << ANSI_FG_RESET << ANSI_BOLD_ON << entry << ANSI_BOLD_OFF << std::endl;
     }
+    const int n = i;
 
-    std::cout << std::endl;
-    std::cout << "Entrez ici votre sélection : " << ANSI_FG_BLUE;
+    out << std::endl;
+    out << "Entrez ici votre sélection : " << ANSI_FG_BLUE;
 
-    std::cout << 4;
+    auto res = readInteger();
+    out << ANSI_FG_RESET;
+    out << std::endl;
 
-    std::cout << ANSI_FG_RESET;
-    std::cout << std::endl;
-    return 0;
+    if (!res.valid || res.value <= 1 || res.value > n)
+    {
+        out << ANSI_FG_RED << "Entrée invalide, veuillez entrer un entier entre " << 1 << " et " << n << "." << ANSI_FG_RESET << std::endl;
+        return InvalidMenuChoice;
+    }
+    else
+    {
+        return res.value;
+    }
 }
 } // namespace UserInterface
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,21 @@
 
 int main()
 {
-    std::cout << "Welcome to AirWatcher\n";
+    UserInterface::banner("Bienvenue dans AirWatcher", true);
+    std::cout << std::endl;
 
-    std::vector<std::string> menu1 = {"Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Iota", "Lambda", "Mumu", "Nono", "Wallah", "Poulet braisé", "Cuisine ouzbèke"};
-    UserInterface::menu("Menu Principal", menu1);
+    std::vector<std::string>
+        menu1 = {"Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Iota", "Lambda", "Mumu", "Nono", "Wallah", "Poulet braisé", "Cuisine ouzbèke"};
+    const int choice = UserInterface::menu("Menu Principal", menu1);
+
+    if (choice == UserInterface::InvalidMenuChoice)
+    {
+        std::cout << "Vous n'avez pas choisi de menu." << std::endl;
+    }
+    else
+    {
+        std::cout << "Vous avez choisi le menu '" << menu1[choice] << "'" << std::endl;
+    }
+
     return 0;
 }
