c877fedd56e6a9f7c00fd17f9189d3637f19a442
Emma <emmaneiss2@gmail.com>
2020-05-26 15:40:08 +0200

    ServiceAnalyste

---
+++ src/Services/ServiceAnalyste.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emman on 26/05/2020.
+//
+
+#include "ServiceAnalyste.h"
---
+++ src/Services/ServiceAnalyste.h
@@ -0,0 +1,19 @@
+//
+// Created by emman on 26/05/2020.
+//
+
+#ifndef INC_3IF_GL_TP_SERVICEANALYSTE_H
+#define INC_3IF_GL_TP_SERVICEANALYSTE_H
+
+#include <map>
+
+class ServiceAnalyste {
+
+    public:
+       Map<Type, Double> agregerDonnees(Coordonnees centre, double rayon, date début, date fin);
+
+
+};
+
+
+#endif //INC_3IF_GL_TP_SERVICEANALYSTE_H
