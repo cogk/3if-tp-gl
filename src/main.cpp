cf867f5c70cb1ff293c6932cb1b05de3441cff0c
Dironiil <berthomet.g@dbmail.com>
2020-06-02 18:01:22 +0200

    Wololo

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 double distanceGPS(double lat1, double lon1, double lat2, double lon2)
 {
     // https://www.movable-type.co.uk/scripts/latlong.html
-    const double PI = M_PI;
+    const double PI = 3.1415;
 
     // calcul distance entre deux points GPS
     const double R = 6371e3; // rayon de la Terre en mètres
