8be89baad9814b6753e07b426934190801f797e0
Emma <emmaneiss2@gmail.com>
2020-06-03 19:39:09 +0200

    Début service pour la liste des capteurs d'un utilisateur

--- src/Services/ServiceContributeur.h
+++ src/Services/ServiceContributeur.h
@@ -5,6 +5,7 @@
 #ifndef INC_3IF_GL_TP_SERVICECONTRIBUTEUR_H
 #define INC_3IF_GL_TP_SERVICECONTRIBUTEUR_H
 
+#include <list>
 #include "../Metier/Mesure.h"
 #include "../Metier/Type.h"
 #include "../DAO/ContributeurDAO.h"
@@ -14,6 +15,8 @@ class ServiceContributeur {
 public:
 
     static bool envoyerDonnees(const Mesure& donnees);
+
+    list<Capteur*>* getCapteurParUser(string& userID);
 
 };
 
